2698d16db07d26cc42a53a25f3d36e5478bba829
vitateje <vitateje@gmail.com>
2020-06-03 00:54:48 -0300

    script_teste

---
+++ teste_iterasys/src/test/java/base/BaseTests.java
@@ -0,0 +1,31 @@
+package base;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseTests {
+	
+	private static WebDriver driver;
+	protected HomePage homePage;
+	
+	@BeforeAll // primeiro metodo a ser executado - configuração do webdriver
+	public static void inicializar() {
+		System.setProperty("webdriver.chrome.driver", "/home/vitateje/Desktop/projects/selenium_java/teste_iterasys/drivers/chrome/83/chromedriver");
+		driver = new ChromeDriver();
+	}
+	
+	@BeforeEach // carregar antes de cada teste
+	public void carregarPaginaInicial() {
+		driver.get("https://marcelodebittencourt.com/demoprestashop/");
+		homePage = new HomePage(driver);
+	}
+	
+	@AfterAll
+	public static void finalizar() {
+		driver.quit();
+	}
+
+}
